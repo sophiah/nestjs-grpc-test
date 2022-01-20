bb957ed9a054ffbb81335a63884f2fb32e988666
Sophia Hsieh <sophia.hsieh@shopback.com>
2022-01-20 16:59:38 +0800

    cleanpu codes

--- src/tracer.ts
+++ src/tracer.ts
@@ -1,23 +1,10 @@
 import { NodeSDK, NodeSDKConfiguration } from '@opentelemetry/sdk-node';
 import { AsyncLocalStorageContextManager } from '@opentelemetry/context-async-hooks';
 import * as process from 'process';
-import {
-  BatchSpanProcessor,
-  ConsoleSpanExporter,
-  SimpleSpanProcessor,
-} from '@opentelemetry/sdk-trace-base';
-import {
-  OTLPMetricExporter,
-  OTLPTraceExporter,
-} from '@opentelemetry/exporter-otlp-http';
-import { MeterProvider, ConsoleMetricExporter } from '@opentelemetry/metrics';
+import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-base';
 import { Resource } from '@opentelemetry/resources';
 import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
-import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
-import {
-  CollectorTraceExporter,
-  CollectorMetricExporter,
-} from '@opentelemetry/exporter-collector-grpc';
+import { CollectorTraceExporter } from '@opentelemetry/exporter-collector-grpc';
 import { GrpcInstrumentation } from '@opentelemetry/instrumentation-grpc';
 
 const traceCollectorOptions = {
@@ -26,7 +13,8 @@ const traceCollectorOptions = {
 const spanExporter = new BatchSpanProcessor(
   new CollectorTraceExporter(traceCollectorOptions),
 );
-// const spanExporter = new SimpleSpanProcessor(new OTLPTraceExporter());
+
+// import { ConsoleSpanExporter, SimpleSpanProcessor } from '@opentelemetry/sdk-trace-base';
 // const spanExporter = new SimpleSpanProcessor(new ConsoleSpanExporter());
 
 const _config: Partial<NodeSDKConfiguration> = {
@@ -37,13 +25,7 @@ const _config: Partial<NodeSDKConfiguration> = {
   contextManager: new AsyncLocalStorageContextManager(),
 };
 
-
-if (process.env.enableTracing) {
-  _config.instrumentations = [new GrpcInstrumentation(), getNodeAutoInstrumentations()]
-}
-else {
-  _config.instrumentations = [new GrpcInstrumentation()];
-}
+_config.instrumentations = [new GrpcInstrumentation()];
 
 const otelSDK = new NodeSDK(_config);
 
